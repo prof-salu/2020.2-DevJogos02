ad7f2e8cf1914b219219b789cea3fbd658c38f23
prof-salu <saluorodrigues@gmail.com>
2020-09-04 23:49:16 -0300

    Arquivos aula03
    - Criação do projeto Invasão Zumbi

---
+++ Invasão Zumbi/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject Jogador;
+
+    private Vector3 distanciaParaJogador;
+    void Start()
+    {
+        distanciaParaJogador = transform.position - Jogador.transform.position;
+    }
+
+    
+    void Update()
+    {
+        transform.position = Jogador.transform.position + distanciaParaJogador;
+    }
+}
---
+++ Invasão Zumbi/Assets/Scripts/Personagem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Personagem : MonoBehaviour
+{
+    public float velocidade = 5;
+
+    private Animator animacao;
+
+    
+    void Start()
+    {
+        animacao = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //Posicao ==> Vector3
+
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(movX, 0, movZ);
+
+        transform.Translate(direcao * Time.deltaTime * velocidade);
+
+        animacao.SetBool("Movendo", direcao != Vector3.zero);
+    }
+}
