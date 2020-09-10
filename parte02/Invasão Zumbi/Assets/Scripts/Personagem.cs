a82130ef1a3593d0d8229f42c7d3a3aa213026e9
prof-salu <saluorodrigues@gmail.com>
2020-09-10 13:10:05 -0300

    arquivos aula04

---
+++ parte02/Invasão Zumbi/Assets/Scripts/Personagem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Personagem : MonoBehaviour
+{
+    public float velocidade = 5;
+    private Vector3 direcao;
+
+    private Animator animacao;
+    private Rigidbody rb;
+
+    
+    void Start()
+    {
+        animacao = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //Posicao ==> Vector3
+
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(movX, 0, movZ);
+
+        //transform.Translate(direcao * Time.deltaTime * velocidade);
+
+        
+        animacao.SetBool("Movendo", direcao != Vector3.zero);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (direcao * Time.deltaTime * velocidade));
+    }
+}
---
+++ parte02/Invasão Zumbi/Assets/Scripts/Zumbi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zumbi : MonoBehaviour
+{
+    public GameObject Jogador;
+    public float velocidade = 5;
+    public float distanciaAtaque = 1.5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float distanciaParaJogador = Vector3.Distance(transform.position, Jogador.transform.position);
+        
+        if(distanciaParaJogador > distanciaAtaque)
+        {
+            Vector3 direcao = Jogador.transform.position - transform.position;
+            rb.MovePosition(rb.position + (direcao.normalized * velocidade * Time.deltaTime));
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            rb.MoveRotation(novaRotacao);
+        }        
+    }
+}
