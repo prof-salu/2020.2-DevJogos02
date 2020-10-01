eb0db558479934d2383867b69e72dfa93b8b0dce
prof-salu <saluorodrigues@gmail.com>
2020-10-01 12:41:42 -0300

    arquivos aula07

---
+++ Parte05/Invasão Zumbi/Assets/Scripts/ControlaInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlaInterface : MonoBehaviour
+{
+    public Slider BarraDeVida;
+    public GameObject Jogador;
+    
+    void Start()
+    {
+        BarraDeVida.maxValue = Jogador.GetComponent<Personagem>().Vida;
+    }
+
+   public void AtualizaBarraDeVida()
+    {
+        BarraDeVida.value = Jogador.GetComponent<Personagem>().Vida;
+    }
+}
---
+++ Parte05/Invasão Zumbi/Assets/Scripts/Personagem.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Personagem : MonoBehaviour
+{
+    public int Vida = 100;
+    public float velocidade = 5;
+    public LayerMask MascaraDoChao;
+    //Precisa fazer o import do UI
+    public GameObject TextoGameOver;
+    public ControlaInterface InterfaceJogo;
+
+    public bool EstaVivo;
+
+    private Vector3 direcao;
+    private Animator animacao;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        animacao = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        EstaVivo = true;
+        //Vida = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //Posicao ==> Vector3
+
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(movX, 0, movZ);
+
+        //transform.Translate(direcao * Time.deltaTime * velocidade);
+
+        
+        animacao.SetBool("Movendo", direcao != Vector3.zero);
+
+        if(EstaVivo == false)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                ReiniciarPartida();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Movimentacao do Personagem
+        rb.MovePosition(rb.position + (direcao * Time.deltaTime * velocidade));
+
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
+
+        RaycastHit impacto;
+
+        if(Physics.Raycast(raio, out impacto, 100, MascaraDoChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+
+            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
+
+            rb.MoveRotation(novaRotacao);
+        }
+    }
+
+    private void ReiniciarPartida()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Fase01");
+    }
+
+    public void TomarDano(int dano)
+    {
+        Vida = Vida - dano;
+        InterfaceJogo.AtualizaBarraDeVida();
+        Debug.Log("Vida atual: " + Vida);
+        GameOver();
+    }
+
+    private void GameOver()
+    {
+        if(Vida <= 0)
+        {
+            TextoGameOver.SetActive(true);
+            Time.timeScale = 0;
+            EstaVivo = false;
+        }
+    }
+}
