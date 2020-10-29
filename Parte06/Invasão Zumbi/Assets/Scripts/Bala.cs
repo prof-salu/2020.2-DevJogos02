56c79deaca8eed65b82469b0919759467a255b9a
prof-salu <saluorodrigues@gmail.com>
2020-10-29 15:00:04 -0300

    arquivos aula07

---
+++ Parte06/Invasão Zumbi/Assets/Scripts/Bala.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float Velocidade = 30;
+
+    public AudioClip SomDeMorte;
+
+    private Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (transform.forward * Velocidade * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Inimigo")){
+            Destroy(other.gameObject);
+            //Toca o audio da morte do inimigo
+            ControlaAudio.Instancia.PlayOneShot(SomDeMorte);
+        }
+        
+        Destroy(gameObject);
+    }
+}
